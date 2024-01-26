9092db61363055eef6769004c2ac9b178a4ebd9c
MASON-SEA <58289388+MASON-SEA@users.noreply.github.com>
2024-01-26 14:54:05 +0800

    Update index.js

--- components/index.js
+++ components/index.js
@@ -41,3 +41,26 @@ import './router/index.js';
 import './layout/index.js';
 import './page/index.js';
 import './cache/index.js';
+
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("/service_worker.js", {
+                scope: "/",
+            });
+            if (registration.installing) {
+                console.log("正在安装 Service worker");
+            } else if (registration.waiting) {
+                console.log("已安装 Service worker installed");
+            } else if (registration.active) {
+                console.log("激活 Service worker");
+            }
+        } catch (error) {
+            console.error(`注册失败：${error}`);
+        }
+    }
+};
+
+// …
+
+registerServiceWorker();
