393d44dc5e59c89f218e79d0678ea0b767c97fb3
MASON-SEA <yangyangmason@outlook.com>
2024-01-26 18:20:29 +0800

    PWA

--- service_worker.js
+++ service_worker.js
@@ -108,16 +108,39 @@ self.addEventListener("install", (event) => {
     );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                    // It can update the cache to serve updated content on the next request
-                    if(cachedResponse) {
-                        console.log(cachedResponse);
-                    }
-                    return cachedResponse || fetch(event.request);
-                }
-            )
-    )
+
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) {
+        return cachedResponse;
+    }
+    try {
+        const networkResponse = await fetch(request);
+        if (networkResponse.ok) {
+            const cache = await caches.open("v1");
+            cache.put(request, networkResponse.clone());
+        }
+        return networkResponse;
+    } catch (error) {
+        return Response.error();
+    }
+}
+
+// self.addEventListener("fetch", event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(cachedResponse => {
+//                     // It can update the cache to serve updated content on the next request
+//                     if(cachedResponse) {
+//                         console.log(cachedResponse);
+//                     }
+//                     return cachedResponse || fetch(event.request);
+//                 }
+//             )
+//     )
+// });
+
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
 });
